7b905e7c9da8ebee107b2c20f1aa53d2d942e2f9
nthngspcl <91619268+nthngspcl@users.noreply.github.com>
2023-04-08 05:15:59 +0600

    08-04

--- src/users/UserList.js
+++ src/users/UserList.js
@@ -1,45 +1,4 @@
-// import React, { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { useGetUsersQuery } from "../api/userApi";
 
-// function UserList() {
-//   const dispatch = useDispatch();
-//   const users = useSelector((state) => state.users.users);
-//   const usersStatus = useSelector((state) => state.users.status);
-//   const error = useSelector((state) => state.users.error);
-
-//   const { data, isFetching } = useGetUsersQuery();
-
-//   useEffect(() => {
-//     if (usersStatus === "idle") {
-//       dispatch(useGetUsersQuery);
-//     }
-//   }, [usersStatus, dispatch]);
-
-//   let content;
-
-//   if (usersStatus === "loading") {
-//     content = <div>Loading...</div>;
-//   } else if (usersStatus === "succeeded") {
-//     content = data.map((user) => (
-//       <div key={user.id}>
-//         <h2>{user.name}</h2>
-//         <p>{user.email}</p>
-//       </div>
-//     ));
-//   } else if (usersStatus === "failed") {
-//     content = <div>{error}</div>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>Users</h1>
-//       {content}
-//     </div>
-//   );
-// }
-
-// export default UserList;
 import { useGetUsersQuery } from '../api/usersApi';
 
 const UserList = () => {
